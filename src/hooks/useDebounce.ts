8b1e0a07b6600c092b32f65cb51f89b2aa260314
Peter Kerfoot <peterckerfoot@gmail.com>
2025-12-18 10:53:59 -0600

    created useDebounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+export default function useDebounce<T>(value: T, delayMs: 300) {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    // after delayMs, update the debounced value to the latest value
+    const timerId = window.setTimeout(() => {
+      setDebounceValue(value)
+    }, delayMs)
+
+    // cleanup: if value changes before the delay ends, cancel the previous timer
+    return () => {
+      window.clearTimeout(timerId)
+    }
+  }, [value, delayMs])
+
+  return debounceValue
+}
